dc4552511ba2ee20d89e5dde0951dd63401649af
Mrhalf2 <73257043+Mrhalf2@users.noreply.github.com>
2020-12-25 17:39:14 +0800

    Create ph.py

---
+++ ph.py
@@ -0,0 +1,258 @@
+try:
+	import webbrowser
+	from time import sleep
+	import  time
+	from tqdm import tqdm, trange
+except half as x:	
+	print("Excited manuod amputa!")
+class half:
+    GRAY = '\033[90m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PINK  = '\033[95m'
+    SBLUE = '\033[96m'
+    
+
+print(half.GREEN+"""\n  FREE PORN SITES\n""")
+print(half.SBLUE+"  •PORNHUB")
+sleep(.10)
+print("  •PORNTUBE")
+sleep(.10)
+print("  •REDTUBE")
+sleep(.10)
+print("  •XVIDEOS")
+sleep(.10)
+print("  •YOUPORN")
+sleep(.10)
+print("  •BEEG")
+sleep(.10)
+print("  •YOUJIZ")
+sleep(.10)
+print("  •XNXX")
+sleep(.10)
+print("  •HENTAI")
+sleep(.10)
+print("  •XXX")
+sleep(.10)
+print("  •KATORSEX")
+sleep(.10)
+print("  •Type [ half ] to View all sites")
+sleep(.75)
+while True:
+	x = input(half.GREEN+"\n  Enter Porn Site : ")
+	if x == ('\nPORNHUB'):
+		x == webbrowser.open('https://pornhub.com')
+	if x == ('PORNTUBE'):
+		x == webbrowser.open('https://porntube.com')
+	if x == ('REDTUBE'):
+		x == webbrowser.open('https://redtube.com')
+	if x == ('XVIDEOS'):
+		x == webbrowser.open('https://xvideos.com')
+	if x == ('YOUPORN'):
+		x == webbrowser.open('https://youporn.com')
+	if x == ('BEEG'):
+		x == webbrowser.open('https://beeg.com')
+	if x == ('YOUJIZ'):
+		x == webbrowser.open('https://youjiz.com')
+	if x == ('XNXX'):
+		x == webbrowser.open('https://xnxx.com')
+	if x == ('HENTAI'):
+		x == webbrowser.open('https://hentai.com')
+	if x == ('XXX'):
+		x == webbrowser.open('https://xxx.com')
+	if x == ('KATORSEX'):
+		x == webbrowser.open('https://katorsex.com')
+	if x == ('half'):
+		print(half.GRAY+"""
+	PornHub
+	PornTube
+	ePorner
+	PornHD
+	Porn
+	xHamster
+	Motherless
+	GotPorn
+	RedTube
+	4Tube
+	Vporn
+	xVideos
+	YouPorn
+	Beeg
+	CollectionOfPorn
+	SpankBang
+	3Movs
+	Tube8
+	YouJizz
+	DrTuber
+	TXXX
+	XNXX
+	ClipHunter
+	PornTrex
+	YourPorn
+	PornsLand
+	VE-Porn
+	Vidz7
+	PornQD
+	VRporn
+	Bobs-Tube
+	XXXstreams
+	YesPornPlease
+	YouFreePornTube
+	Porn4Days
+	PornHD8k
+	UltraHorny
+	PornHD6k
+	WatchXXXFree
+	PlusOne8
+	TnaFlix
+	PornRewind
+	Sweext
+	xTapes
+	Vidz24
+	RushPorn
+	HDpornstarz
+	CumLouder
+	Fux
+	PornPremieres
+	LuxureTV
+	PervertSlut
+	PornCorn
+	PornHD3x
+	xKeezMovies
+	Bubble
+	SexGalaxy
+	XOpenload
+	NetPornSex
+	HD-EasyPorn
+	Palimas
+	PornBraze
+	SexTVx
+	Sexu
+	PhicaTube
+	YourDailyPornVideos
+	PornoVideosHub
+	ShameLess
+	HandJobHub
+	ToBePorn
+	YoungPornVideos
+	AlotPorn
+	XXvideoss
+	JizzMan
+	Slutload
+	PandaMovie
+	PornDish
+	StreamingPorn
+	RedPorn
+	XXXstreams(dot)ey
+	FullXXXMovies
+	HDpornFull
+	PussySpace
+	GameOfPorn
+	TGPdog
+	PornStreams
+	PornoBae
+	PornVibe
+	PalmTube
+	FAkingsTV
+	DVDtrailerTube
+	JAVfinder
+	StreamJAV
+	JAVwhores
+	vJAV
+	JAVdoe
+	HPjav
+	JAVQD
+	JAVfun
+	WatchJAVonline
+	JAVseen
+	JAVout
+	OhYeah1080
+	JAV(dot)guru
+	JAVhd3x
+	SexTop
+	ShyAV
+	Vpondo
+	JAVleak
+	JAVhd4k
+	JAVbraze
+	IVhunter
+	YouAV
+	JAV2be
+	JAV-720p 
+	JAVdude
+	KapLog
+	iYotTube
+	JavHD
+	CrazyAsianGFs
+	AsianGFvideos
+	R18
+	Erito
+	SakuraLive
+	LiveSexAsian
+	Streamate/Asian
+	PornHub/Arab
+	xHamster/Arab
+	xVideos/Arab
+	Motherless/Arab
+	ArabySexy
+	FreeArabSexx
+	AflamNeek
+	SexAraby
+	AflamSexHD
+	AflamPorn
+	SexSaoy
+	ArabGFvideos
+	TourOfBooty
+	BlackGFS
+	RoundAndBrown
+	BrownBunnies
+	MyEbonyGF
+	BlackIsBetter
+	PornHub/Black
+	MyFreeBlack
+	ShesFreaky
+	xHamster/Black
+	HomeGrownFreaks
+	EmpressLeak
+	SenePorno
+	HoodAmateurs
+	GhettoTube
+	SheGotAss
+	YsPorn
+	PornHub/Indian
+	IndianPornVideos
+	xHamster/Indian
+	FreeSexyIndians
+	DesiXNXX
+	SuckSex
+	FreeHDx
+	ClipsAge
+	SexTube
+	IndianGFVideos
+	TheIndianPorn
+	FuckMyIndianGF
+	DesiPapa
+	IndianHiddenCams
+	Pornolandia
+	PornoCarioca
+	PornHub/Latina
+	xHamster/Latina
+	CNNamador
+	MamaSiJaYa
+	PornoGratis
+	BrasilTudoLiberado
+	ColegialasDeVe
+	XnXX
+	Faketaxi
+	Youjiz
+	Beeg
+	2 cup 1 girl
+	1 man 1 jar
+	Porntangina
+	Eutangina
+	Boldbogan
+	Perpetual student
+	Hentai
+		""")
